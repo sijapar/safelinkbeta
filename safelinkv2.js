8809b409730ddc9c8ff6782e72167eb13ed04ba1
sijapar <akunjapar@gmail.com>
2019-07-18 00:08:47 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -19,14 +19,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*6)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="essential-things-to-change-in-insurance.html"}
-if(x=="2"){xxx="why-real-estate-title-agents-need.html"}
-if(x=="3"){xxx="why-telematics-is-major-disruptor-in.html"}
-if(x=="4"){xxx="devops-automation-for-faster-and.html"}
-if(x=="5"){xxx="telematics-and-big-data-next-generation.html"}
-if(x=="6"){xxx="never-compare-insurance-based-on-price.html"}a_to_ve[i].href="https://beautytricksnew.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="top-10-beautiful-app-for-android-rooted.html"}
+if(x=="2"){xxx="the-best-top-10-worlds-insurance.html"}
+if(x=="3"){xxx="top-seven-7-best-auto-insurance.html"}
+if(x=="4"){xxx="top-25-propertycasualty-insurers-in-us.html"}
+output.value = "https://safelinkbeta.blogspot.com/2017/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
